627da003014aca961b71ccb4442a7aa4009d709c
alexBSommer <38224684+alexBSommer@users.noreply.github.com>
2018-04-23 11:21:17 -0400

    A branch to test palindromes
    Unknown how this will interfere with existing files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PalindromeLibrary;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a String to check for palindromes");
+            Candidate userEntry = new Candidate(Console.ReadLine());
+            if (userEntry.status)
+            {
+                Console.WriteLine(userEntry.entry + " is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine(userEntry.entry + " is not a Palindrome.");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ PalindromeTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PalindromeTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PalindromeTester")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d757e648-978c-425d-a9c1-5fc1226c2ab3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
